9943c9861e6136789be33e14f2098ef45d67f01f
Daniel Kim <d95kim@gmail.com>
2021-10-15 17:33:47 -0700

    Finished cookieParser. working on auth.js create session

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,7 +2,26 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  var session = {};
+  models.Sessions.
+    req.sessions = session;
+  next();
 };
+
+
+
+
+//1. access our parsed cookies
+//2. looks up the user data related to THAT session
+//3.
+
+// createSession(requestWithoutCookies, response, function() {
+//   var session = requestWithoutCookies.session;
+//   expect(session).to.exist;
+//   expect(session).to.be.an('object');
+//   expect(session.hash).to.exist;
+//   done();
+// });
 
 /************************************************************/
 // Add additional authentication middleware functions below
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,25 @@
 const parseCookies = (req, res, next) => {
+  //console.log('req', req);
+  var cookies = {};
+  if (req.headers.cookie) {
+    var parssedCookiesArr = req.headers.cookie.split('; ');
+    parssedCookiesArr.forEach(cookie => {
+      var preCookieObj = cookie.split('=');
+      cookies[preCookieObj[0]] = preCookieObj[1];
+      //console.log(cookies);
+    });
+  }
+  req.cookies = cookies;
+  next();
 };
 
 module.exports = parseCookies;
+
+
+//shortlyid=18ea4fb6ab3178092ce936c591ddbb90c99c9f66
+
+//shortlyid: '18ea4fb6ab3178092ce936c591ddbb90c99c9f66'
+
+// cookies = {
+//   shortlyid: '18ea4fb6ab3178092ce936c591ddbb90c99c9f66'
+// }
